5a97dd9ea1b9245119da9dad61f5683abcbda701
Alex <AkJones2007@gmail.com>
2016-01-13 17:48:43 -0500

    Adds functionality to clear placeholder input while filling out the form Adds styling for both the placeholder, and when the field is active/populated

---
+++ js/actions.js
@@ -0,0 +1,24 @@
+$(function() {
+
+  $('.field').on('focus', function(event) {
+    event.preventDefault();
+    var placeholder = $(this).attr('placeholder');
+
+    $(this).addClass('active');
+    $(this).attr('placeholder', '');
+
+    if($(this).val() === '') {
+      $(this).removeClass('populated');
+    }
+
+    $(this).on('blur', function(event) {
+      $(this).removeClass('active');
+      $(this).attr('placeholder', placeholder);
+
+      if($(this).val() !== '') {
+        $(this).addClass('populated');
+      }
+    });
+  });
+
+});
